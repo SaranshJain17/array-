1fcb259e0ec13572a287f207cda7647c1760dd4d
SaranshJain17 <thesaranshjain@gmail.com>
2024-07-16 11:19:30 +0530

    Add files via upload

---
+++ arraymax.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int array[]={4,5,7,9};
+    int max=array[0];
+    for(int i=1;i<4;i++){
+
+       if(array[i]>max);
+       max =array[i];
+
+
+    }
+cout<<max<<endl;
+
+        
+
+
+    
+
+}
